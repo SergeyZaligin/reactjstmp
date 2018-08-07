ef16a2e77b7d7bdbe2d8d2451026cd6be6e52e3a
Sergey Zaligin <webdevjsphp@gmail.com>
2018-08-07 18:13:18 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { setBooks } from './actions/books';
 
 class App extends Component {
   
   //const books = this.props.books.books;
   
   render() {
-  	console.log(this.props);
+  	const { books } = this.props.books;
+    const { setBooks } = this.props;
+    const newBooks = [
+      {
+        id: 0,
+        title: 'Hz'
+      }
+    ];
     return (
       <div className="container">
-      	<h1>Hello world!!!</h1>	 
+      	<h1>{books[0].title}</h1>	 
+        <button onClick={setBooks.bind(this, newBooks)}>Load</button>
       </div>
     );
   }
@@ -19,5 +28,8 @@ const mapState = state => ({
 	...state
 });
 
+const mapDispatchToProps = dispatch => ({
+  setBooks: books => dispatch(setBooks(books))
+});
 
-export default connect(mapState)(App);
+export default connect(mapState, mapDispatchToProps)(App);
